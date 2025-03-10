18ee28f16a682c42df5194a2df935e560bd933dc
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-10 10:51:51 +0200

    added industries grid for mobile

--- src/components/homepage/industries/Industries.tsx
+++ src/components/homepage/industries/Industries.tsx
@@ -1,3 +1,14 @@
+import { useTranslations } from "next-intl";
+import IndustriesGridMob from "./IndustriesGridMob";
+
 export default function Industries() {
-  return <div>Industries</div>;
+  const t = useTranslations("homePage.industries");
+  return (
+    <section className="container max-w-[1920px] py-[100px] xl:py-[110px] bg-white">
+      <h2 className="mb-12 font-prosto uppercase text-center text-20reg xl:text-36reg leading-[100%] text-black">
+        {t("title")}
+      </h2>
+      <IndustriesGridMob />
+    </section>
+  );
 }
