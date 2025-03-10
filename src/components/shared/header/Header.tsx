0b0fd72669f5a54dba0efca1db1aed98d8865bb2
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-10 22:11:32 +0200

    added locale switcher

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,10 +1,12 @@
+import LocaleSwitcher from "./LocaleSwitcher";
 import Logo from "./Logo";
 
 export default function Header() {
   return (
     <header className="py-6">
-      <div className="container max-w-[1920px]">
+      <div className="flex justify-between container max-w-[1920px]">
         <Logo />
+        <LocaleSwitcher />
       </div>
     </header>
   );
