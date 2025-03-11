0696fbcf0291004f51262ccdc3d90969dc24a771
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-11 19:48:24 +0200

    added team list

--- src/components/homepage/team/Team.tsx
+++ src/components/homepage/team/Team.tsx
@@ -1,7 +1,18 @@
+import { useTranslations } from "next-intl";
+import TeamList from "./TeamList";
+
 export default function Team() {
+  const t = useTranslations("homePage.team");
   return (
-    <section id="team" className="scroll-mt-12">
-      Team
+    <section
+      id="team"
+      className="container max-w-[1920px] scroll-mt-12 py-[100px] xl:py-[180px]"
+    >
+      <h2 className="max-w-[758px] mb-6 xl:mb-7 font-prosto text-20reg xl:text-36reg uppercase">
+        {t("title")}
+      </h2>
+      <p className="max-w-[442px] mb-[53px] xl:mb-[83px]">{t("description")}</p>
+      <TeamList />
     </section>
   );
 }
