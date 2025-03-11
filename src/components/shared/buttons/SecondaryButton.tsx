c3e41789c74e45d3d6dfa8fcd1f1de8e975df090
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-11 10:49:21 +0200

    added button hover

--- src/components/shared/buttons/SecondaryButton.tsx
+++ src/components/shared/buttons/SecondaryButton.tsx
@@ -10,13 +10,12 @@ export default function SecondaryButton({
     <button
       onClick={onClick}
       className={`group relative overflow-hidden flex items-center justify-center py-[10px] px-4 text-14reg rounded-full transition duration-300 ease-out border border-white active:scale-95 
-         xl:hover outline-none 
+      
         ${className}`}
     >
-      {" "}
       <span
-        className="absolute top-0 left-[-150%] w-[100%] h-full bg-gradient-to-r from-white/20 via-[#12DDF3]/30 to-white/20 opacity-70 
-  skew-x-[-40deg] group-hover:left-[120%] transition-all duration-[800ms] ease-in-out"
+        className="absolute top-0 left-[-150%] w-full h-full bg-gradient-to-r from-white/20 via-[#12DDF3]/30 to-white/20 opacity-70 
+  skew-x-[-40deg] group-[focus-visible]:left-[120%] xl:group-hover:left-[120%] transition-all duration-[800ms] ease-in-out"
       />
       {children}
     </button>
