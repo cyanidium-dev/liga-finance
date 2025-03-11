96efc498785a449326c7293a22f2aa3a845cf9c8
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-11 15:50:26 +0200

    added footer texts

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -1,3 +1,11 @@
+import Logo from "../logo/Logo";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="w-dvw bg-main pt-[50px] pb-10">
+      <div className="container max-w-[1920px]">
+        <Logo className="w-6 xl:w-[45px] h-auto" />
+      </div>
+    </footer>
+  );
 }
--- src/components/shared/logo/Logo.tsx
+++ src/components/shared/logo/Logo.tsx
@@ -2,10 +2,20 @@ import Image from "next/image";
 import { companyName } from "@/constants/constants";
 import { Link } from "@/i18n/navigation";
 
-export default function Logo() {
+interface LogoProps {
+  className: string;
+}
+
+export default function Logo({ className }: LogoProps) {
   return (
     <Link href="/" className="flex gap-2 xl:gap-[10px] items-center">
-      <Image src="/images/icons/logo.svg" alt="logo" width="24" height="26" />
+      <Image
+        src="/images/icons/logo.svg"
+        alt="logo"
+        width="24"
+        height="26"
+        className={className}
+      />
       <p className="font-prosto uppercase text-12reg xl:text-14reg">
         {companyName}
       </p>
