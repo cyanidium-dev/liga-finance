2970faf3c8303448fb3c8f8ea682bc5d93b1b50b
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-12 15:39:56 +0200

    updated main button hover

--- src/components/shared/buttons/MainButton.tsx
+++ src/components/shared/buttons/MainButton.tsx
@@ -9,12 +9,13 @@ export default function MainButton({
   return (
     <button
       onClick={onClick}
-      className={`group flex items-center justify-center p-[1px] rounded-full bg-mainButtonBorderGradient transition duration-300 ease-in-out active:scale-95 ${className}`}
+      className={`group relative overflow-hidden flex items-center justify-center p-[1px] rounded-full bg-mainButtonBorderGradient transition duration-300 ease-in-out active:scale-95 ${className}`}
     >
-      <div
-        className="w-full h-full px-4 py-[10px] rounded-full bg-mainButtonGradient text-14med text-lightBlue focus-visible:brightness-[115%] 
-      xl:hover:brightness-[115%] transition duration-300 ease-in-out"
-      >
+      <div className="w-full h-full px-4 py-[10px] rounded-full bg-mainButtonGradient text-14med text-lightBlue transition duration-300 ease-in-out">
+        <span
+          className="absolute top-0 left-[-150%] w-full h-full bg-gradient-to-r from-white/20 via-blue/30 to-white/20 opacity-70 
+  skew-x-[-40deg] group-[focus-visible]:left-[120%] xl:group-hover:left-[120%] transition-all duration-[800ms] ease-in-out"
+        />
         {children}
       </div>
     </button>
