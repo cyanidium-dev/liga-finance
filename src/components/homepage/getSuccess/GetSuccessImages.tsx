443c235fc3c60935c5aba634a54d8ca59fe534ed
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-13 13:36:28 +0200

    updated get success section images

--- src/components/homepage/getSuccess/GetSuccessImages.tsx
+++ src/components/homepage/getSuccess/GetSuccessImages.tsx
@@ -3,28 +3,21 @@ import Image from "next/image";
 export default function GetSuccessImages() {
   return (
     <>
-      <Image
-        src="/images/getSuccess/building.webp"
-        alt="building"
-        width={2560}
-        height={1286}
-        className="absolute -z-40 top-0 left-0 w-full h-full object-cover"
-      />
-      <div className="block absolute z-[-5] tab:-z-30 left-[-33px] bottom-[-100px] tab:bottom-[-506px] size-[400px] tab:size-[801px] bg-main blur-[180px] supports-[backdrop-filter]:blur-[180px] will-change-transform"></div>
+      <div className="block absolute z-[-5] tab:-z-30 left-[-33px] bottom-[-220px] tab:bottom-[-604px] size-[400px] tab:size-[801px] bg-main blur-[180px] supports-[backdrop-filter]:blur-[180px] will-change-transform"></div>
 
       <Image
         src="/images/getSuccess/logo.webp"
         alt="logo"
         width={927}
         height={927}
-        className="block absolute -z-10 bottom-[-60px] right-[-60px] tab:right-auto tab:bottom-[-83px] tab:left-[-175px] lg:left-[-225px] w-[230px] tab:w-[488px] lg:w-[628px] h-auto"
+        className="block absolute -z-10 bottom-[-180px] right-[-60px] tab:right-auto tab:bottom-[-181px] tab:left-[-175px] lg:left-[-225px] w-[230px] tab:w-[488px] lg:w-[628px] h-auto"
       />
       <Image
         src="/images/getSuccess/woman.webp"
         alt="woman"
         width={952}
         height={1172}
-        className="hidden lg:block absolute -z-10 bottom-0 left-[89px] w-[476px] h-auto"
+        className="hidden lg:block absolute -z-10 bottom-[-98px] left-[89px] w-[476px] h-auto"
       />
     </>
   );
