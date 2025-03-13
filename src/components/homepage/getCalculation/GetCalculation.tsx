02f41439a1b2a601401f26cbf0171acdda48b97a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-13 13:18:05 +0200

    updated get calculation section

--- src/components/homepage/getCalculation/GetCalculation.tsx
+++ src/components/homepage/getCalculation/GetCalculation.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 import GetCalculationImages from "./GetCalculationImages";
 import CallBackApplication from "@/components/shared/callBackApllication/CallBackApplication";
 
@@ -7,17 +8,25 @@ export default function GetCalculation() {
 
   return (
     <section className="relative pt-[140px] pb-[120px] tab:py-[98px] overflow-hidden">
-      <GetCalculationImages />
-      <div className="container max-w-[1280px]">
+      <Image
+        src="/images/getCalculation/building.webp"
+        alt="building"
+        width={2560}
+        height={1286}
+        className="absolute -z-40 top-0 left-0 w-full h-full object-cover"
+      />
+
+      <div className="relative container max-w-[1280px]">
         <div className="max-w-[520px] tab:max-w-[376px] desk:max-w-[520px] mx-auto tab:mx-0">
-          <h2 className="mb-6 xl:mb-7 font-prosto uppercase text-center tab:text-left text-24reg xl:text-32reg leading-[130%]">
+          <h2 className="mb-6 xl:mb-7 font-prosto uppercase text-center tab:text-left text-24reg xl:text-32reg leading-[125%]">
             {t("title")}
           </h2>
-          <p className="mb-8 xl:mb-12 text-center tab:text-left">
+          <p className="mb-8 xl:mb-12 text-center tab:text-left leading-[125%]">
             {t("description")}
           </p>
           <CallBackApplication />
         </div>
+        <GetCalculationImages />
       </div>
     </section>
   );
