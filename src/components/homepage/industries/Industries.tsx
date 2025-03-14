08edab29f17f37d5ada8b120b18d3fdf899e6e4a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-14 17:45:25 +0200

    added industries section animation

--- src/components/homepage/industries/Industries.tsx
+++ src/components/homepage/industries/Industries.tsx
@@ -1,6 +1,9 @@
 import { useTranslations } from "next-intl";
 import IndustriesGridMob from "./IndustriesGridMob";
 import IndustriesGridDesk from "./IndustriesGridDesk";
+import AnimatedWrapper from "@/components/shared/animatedWrapper/AnimatedWrapper";
+import { fadeInAnimation } from "@/helpers/animation";
+import { motion } from "framer-motion";
 
 export default function Industries() {
   const t = useTranslations("homePage.industries");
@@ -11,9 +14,13 @@ export default function Industries() {
       className="py-[100px] xl:py-[110px] scroll-mt-12 bg-white"
     >
       <div className="container max-w-[1280px] ">
-        <h2 className="mb-12 font-prosto uppercase text-center text-20reg xl:text-36reg leading-[100%] text-black">
+        <AnimatedWrapper
+          className="mb-12 font-prosto uppercase text-center text-20reg xl:text-36reg leading-[100%] text-black"
+          as={motion.h2}
+          animation={fadeInAnimation({ y: 50 })}
+        >
           {t("title")}
-        </h2>
+        </AnimatedWrapper>
         <IndustriesGridMob />
         <IndustriesGridDesk />
       </div>
