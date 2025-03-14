8b6a1077a1bab17ffd9c5820c13d1c90c41f417e
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-14 16:10:15 +0200

    animated services list

--- src/components/homepage/services/Services.tsx
+++ src/components/homepage/services/Services.tsx
@@ -1,5 +1,8 @@
 import { useTranslations } from "next-intl";
 import ServicesList from "./ServicesList";
+import AnimatedWrapper from "@/components/shared/animatedWrapper/AnimatedWrapper";
+import { motion } from "framer-motion";
+import { listVariants } from "@/helpers/animation";
 
 export default function Services() {
   const t = useTranslations("homePage.services");
@@ -39,11 +42,15 @@ export default function Services() {
 
   return (
     <section id="services" className="flex justify-center scroll-mt-12">
-      <ul className="flex flex-col justify-center tabxl:flex-row gap-5 max-w-[1280px] container py-[100px] lg:py-[110px] text-black">
+      <AnimatedWrapper
+        as={motion.ul}
+        animation={listVariants({ staggerChildren: 0.5 })}
+        className="flex flex-col justify-center tabxl:flex-row gap-5 max-w-[1280px] container py-[100px] lg:py-[110px] text-black"
+      >
         {services.map((servicesList, idx) => (
           <ServicesList key={idx} servicesList={servicesList} />
         ))}
-      </ul>
+      </AnimatedWrapper>
     </section>
   );
 }
