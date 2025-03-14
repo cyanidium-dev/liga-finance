62e3667e7f874bf36d71336d60b0b9df047694ef
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-14 18:17:46 +0200

    animated statistics section

--- src/components/homepage/statistics/StatisticsItem.tsx
+++ src/components/homepage/statistics/StatisticsItem.tsx
@@ -1,3 +1,5 @@
+import AnimatedListItem from "@/components/shared/animatedWrapper/AnimatedListItem";
+
 interface StatisticsItemProps {
   statisticsItem: { count: string; description: string };
 }
@@ -8,13 +10,13 @@ export default function StatisticsItem({
   const { count, description } = statisticsItem;
 
   return (
-    <li className="flex flex-col items-center justify-center w-[47.4%] tab:w-[23.68%] px-3 lg:px-[40px] py-10 lg:py-[66px] text-black text-center bg-statisticsGradient rounded-[8px]">
+    <AnimatedListItem className="flex flex-col items-center justify-center w-[47.4%] tab:w-[23.68%] px-3 lg:px-[40px] py-10 lg:py-[66px] text-black text-center bg-statisticsGradient rounded-[8px]">
       <p className="mb-[18px] lg:mb-6 font-prosto text-32reg lg:text-52reg leading-[100%]">
         {count}
       </p>
       <p className="text-9med lg:text-16med uppercase leading-[100%]">
         {description}
       </p>
-    </li>
+    </AnimatedListItem>
   );
 }
--- src/components/homepage/statistics/StatisticsList.tsx
+++ src/components/homepage/statistics/StatisticsList.tsx
@@ -1,5 +1,8 @@
 import { useTranslations } from "next-intl";
 import StatisticsItem from "./StatisticsItem";
+import AnimatedWrapper from "@/components/shared/animatedWrapper/AnimatedWrapper";
+import { motion } from "framer-motion";
+import { listVariants } from "@/helpers/animation";
 
 export default function StatisticsList() {
   const t = useTranslations("homePage.statistics");
@@ -11,10 +14,15 @@ export default function StatisticsList() {
     { count: "1000+", description: t("consultations") },
   ];
   return (
-    <ul className="flex flex-wrap tab:flex-nowrap justify-between gap-4 tab:gap-5">
+    <AnimatedWrapper
+      as={motion.ul}
+      viewport={{ once: true, amount: 0.4 }}
+      animation={listVariants({ staggerChildren: 0.5 })}
+      className="flex flex-wrap tab:flex-nowrap justify-between gap-4 tab:gap-5"
+    >
       {statisticsList.map((statisticsItem, idx) => (
         <StatisticsItem key={idx} statisticsItem={statisticsItem} />
       ))}
-    </ul>
+    </AnimatedWrapper>
   );
 }
